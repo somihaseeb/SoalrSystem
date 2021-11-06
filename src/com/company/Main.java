39708161bcbfc996046214fb2c1ff8bde6bbb51f
somihaseeb <somihaseeb@gmail.com>
2021-11-06 11:28:47 -0400

    solar system files

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SolarSystem andromeda = new SolarSystem();
+       andromeda.sun=1;
+        andromeda.planet = 2;
+        andromeda.moon =4;
+
+
+
+    }
+}
